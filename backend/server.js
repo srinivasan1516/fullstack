1f17cd9d6887c29da40f499655e56cf6894f7c79
srinivasan1516 <sriniswa1998@gmail.com>
2025-11-19 21:27:02 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 5000;
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from EC2 backend!" });
-});
-
-app.listen(port, () => console.log("Backend running on port " + port));
